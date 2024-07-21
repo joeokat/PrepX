6320415fd6ee8d85fb35feb66f71b95cafefc6d6
Joshua Ofosu <joeokat@gmail.com>
2024-07-21 19:48:16 +0000

    sw

--- sw.js
+++ sw.js
@@ -1,37 +1,3 @@
-// if("ServiceWorker" in navigator){
-//     navigator.serviceWorker.register("sw.js").then(registration => {
-//       console.log("SW Registered:");
-//       console.log(registration);
-//     }).catch(
-//       error => console.error("SW Registration Failed:", error)
-//     )
-//   }
-   
-//  self.addEventListener("install", e => {
-//         self.skipWaiting()
-//         e.waitUntil(
-//             caches.open('v1').then(cache => {
-//                 return cache.addAll([
-//                     '/',
-//                     '/index.html',
-//                     '/styles.css',
-//                     '/app.js',
-//                     '/sw.js',
-//                     '/site.manifest.json',
-//                     '/assets/icon.png',
-//                     '/assets/logo.png',
-//                     '/assets/favicon.ico'
-//                 ])
-//             })
-//         )
-//     });
-
-//     self.addEventListener("fetch", e => {
-//         e.respondWith(
-//             caches.match(e.request) || fetch(e.request)
-//         )
-//     });
-
 const HOSTNAME_WHITELIST = [
     self.location.hostname,
     'fonts.gstatic.com',
